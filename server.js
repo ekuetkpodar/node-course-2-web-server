66808a1b326a0496379cf5f700eaddaad9d3287a
Ekue <ekuetkpodar@gmail.com>
2017-11-04 17:30:31 -0400

    Inital commit

---
+++ server.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const hbs = require("hbs");
+var app = express();
+const fs = require('fs');
+
+
+
+
+app.set("view engine", "hbs");
+app.use(express.static(__dirname + '/public'));
+//hbs.registerpartials(__dirname + "/views/partials")
+hbs.registerPartials(__dirname + '/views/partials');
+
+
+app.use((req,res,next) =>{
+    var now = new Date().toString();
+    var log = `${now}: ${req.method} ${req.url}`;
+    
+    console.log(log);
+    fs.appendFile('server.log',log +'\n')
+    next();
+})
+
+app.use((req,res,next) =>{
+    res.render('maintenance.hbs');
+})
+hbs.registerHelper('getCurrentYear', () =>{
+    return new Date().getFullYear();
+});
+
+app.get('/',(req,res) => {
+   // res.send('hello Express');
+   res.render('home.hbs',{
+        pageTitle:'Home Page',
+       message:'Welcome to our site'
+   })
+})
+
+
+hbs.registerHelper('screamIt',(text) =>{
+    return text.toUpperCase(text);
+})
+
+app.get('/about' , (req,res) => {
+    res.render("about.hbs",{
+        pageTitle:'About Page',
+    });
+})
+
+
+ app.get('/bad', (req,res) => {
+      res.send("Invalid Request");
+ });
+
+app.listen(3000);
